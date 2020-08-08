7463000b18ac1678e511993b60c75f5c2c736ac6
Lucas Silveira <lugano1995@gmail.com>
2020-08-08 13:55:03 -0300

    refactor: use cloneDeep of lodash lib for copy the object repository

--- src/infra/repositories/fake/FakeAppointment.repository.ts
+++ src/infra/repositories/fake/FakeAppointment.repository.ts
@@ -1,5 +1,6 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import faker from 'faker';
+import { cloneDeep } from 'lodash';
 import IAppointment from '@domain/entities/Appointment.interface';
 import IAppointmentRepository from '@domain/protocols/repository/AppointmentRepository.interface';
 import IDateHandler from '@domain/protocols/utils/DateHandler.interface';
@@ -24,7 +25,7 @@ class TypeormAppointmentRepository implements IAppointmentRepository {
       this.dateHandler.isEqual(appmnt.date, date),
     );
 
-    return Promise.resolve(appointment || null);
+    return Promise.resolve(cloneDeep(appointment) || null);
   }
 
   public async create(
@@ -32,7 +33,7 @@ class TypeormAppointmentRepository implements IAppointmentRepository {
   ): Promise<Required<IAppointment>> {
     const newAppointment = { ...appointment, id: faker.random.uuid() };
     this.appointments.push(newAppointment);
-    return Promise.resolve(newAppointment);
+    return Promise.resolve(cloneDeep(newAppointment));
   }
 }
 
--- src/infra/repositories/fake/FakeUser.repository.ts
+++ src/infra/repositories/fake/FakeUser.repository.ts
@@ -1,6 +1,6 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies */
 import faker from 'faker';
-
+import { cloneDeep } from 'lodash';
 import IUser from '@domain/entities/User.interface';
 import IUserRepository from '@domain/protocols/repository/UserRepository.interface';
 
@@ -12,19 +12,19 @@ class FakeUserRepository implements IUserRepository {
   }
 
   public async findAll(): Promise<Required<IUser>[]> {
-    return Promise.resolve(JSON.parse(JSON.stringify(this.users)));
+    return Promise.resolve(this.users);
   }
 
   public async findById(id: string): Promise<Required<IUser> | null> {
     const user = this.users.find(usr => usr.id === id);
 
-    return Promise.resolve(JSON.parse(JSON.stringify(user || '')) || null);
+    return Promise.resolve(cloneDeep(user) || null);
   }
 
   public async findByEmail(email: string): Promise<Required<IUser> | null> {
     const user = this.users.find(usr => usr.email === email);
 
-    return Promise.resolve(JSON.parse(JSON.stringify(user || '')) || null);
+    return Promise.resolve(cloneDeep(user) || null);
   }
 
   public async create(user: IUser): Promise<Required<IUser>> {
@@ -34,13 +34,13 @@ class FakeUserRepository implements IUserRepository {
       id: faker.random.uuid(),
     };
     this.users.push(newUser);
-    return Promise.resolve(JSON.parse(JSON.stringify(newUser)));
+    return Promise.resolve(cloneDeep(newUser));
   }
 
   public async update(user: Required<IUser>): Promise<Required<IUser>> {
     const userIndex = this.users.findIndex(usr => usr.id === user.id);
     this.users[userIndex] = user;
-    return Promise.resolve(user);
+    return Promise.resolve(cloneDeep(user));
   }
 }
 
